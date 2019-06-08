eda586ad4735e6fc1b223ecd2b6cd73e476d769e
IkaroSales <ikarosales7@gmail.com>
2019-06-08 11:53:15 -0300

    Ocultar erro

--- public/index.php
+++ public/index.php
@@ -8,8 +8,8 @@ use Zend\Stdlib\ArrayUtils;
  * to the application root now.
  */
 
-error_reporting(E_ALL | E_STRICT);
-ini_set("display_errors", 1);
+error_reporting(0);
+ini_set(“display_errors”, 0);
 
 chdir(dirname(__DIR__));
 
